88bb6132fea097f86dacf4285b8c4d57ca71bd5b
fahrialfarizki <fahrialfarizki858@gmail.com>
2025-02-19 09:29:54 +0700

    Update NotFound.jsx

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router";
 function NotFound() {
   return (
     <>
-      <h1 className="text-xl font-bold text-center">
+      <h1 className="text-xl font-bold text-center mt-12">
         Halaman Tidak Ditemukan 404
       </h1>
-      <p>
+      <p className="text-center">
         Silakan Kembali Ke Halaman <Link to="/" className="underline">Home</Link>
       </p>
     </>
